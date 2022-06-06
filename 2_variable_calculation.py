235f7f14eff4505224879807241a2c4a89074700
SettingTom <tnbt0277@gmail.com>
2022-06-07 07:51:13 +0900

    プログラム2-4(変数と型)

--- 2_variable_calculation.py
+++ 2_variable_calculation.py
@@ -5,6 +5,18 @@
 # プログラム2-2(コメント文の挿入)
 
 # プログラム2-3(変数と計算)
-x = 10
-y = x + 5
-print(y)
+# x = 10
+# y = x + 5
+# print(y)
+
+# プログラム2-4(変数と型)
+n = 685
+r = 12.9
+c = 6 - 7j
+b = True
+s = "abc"
+print(type(n))
+print(type(r))
+print(type(c))
+print(type(b))
+print(type(s))
