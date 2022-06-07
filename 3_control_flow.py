1b8c1c192828a99ab385cedadab405961c8ca565
SettingTom <tnbt0277@gmail.com>
2022-06-08 07:15:33 +0900

    プログラム3-2(if文の使用例(2))

--- 3_control_flow.py
+++ 3_control_flow.py
@@ -3,3 +3,13 @@
 # if x >= 10 and x < 100:
 #   print(x)
 # print('実行終了')
+
+# プログラム3-2(if文の使用例(2))
+# n=16
+# m=11
+# if n > m:
+#   print(n)
+# elif n < m:
+#   print(m)
+# else:
+#   print('等しい値です')
