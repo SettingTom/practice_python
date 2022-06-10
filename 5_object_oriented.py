522602983d533cce82446f40253b3cb39fb20cf5
SettingTom <tnbt0277@gmail.com>
2022-06-10 23:43:14 +0900

    プログラム5-4(クラスの定義と利用)

--- 5_object_oriented.py
+++ 5_object_oriented.py
@@ -35,3 +35,20 @@
 # print(s.count('bc'))
 # sp = s.split('b')
 # print(sp)
+
+# プログラム5-4(クラスの定義と利用)
+# import math
+
+# class Point:
+#   def __init__(self, x, y):
+#     self.x = x
+#     self.y = y
+#   def get_distance(self, p):
+#     return math.sqrt((self.x - p.x)**2 + (self.y - p.y)**2)
+#   def get_midpoint(self, p):
+#     return Point((self.x + p.x)/2, (self.y + p.y)/2)
+# p1 = Point(5, 7)
+# p2 = Point(2, 3)
+# print(p1.get_distance(p2))
+# mp = p1.get_midpoint(p2)
+# print(str(mp.x) + ',' + str(mp.y))
