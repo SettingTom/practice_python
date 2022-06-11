59a2141992c1b7588725189d032c4def533e576c
SettingTom <tnbt0277@gmail.com>
2022-06-11 14:12:53 +0900

    プログラム7-1(素数判定のプログラム)

---
+++ 7_integer_calculation.py
@@ -0,0 +1,13 @@
+# プログラム7-1(素数判定のプログラム)
+# def isPrime(n):
+#   if n < 2:
+#     return False
+#   for i in range(2, n):
+#     if n % i == 0:
+#       return False
+#   return True
+# p = int(input("input number:"))
+# if isPrime(p):
+#   print("素数である")
+# else:
+  # print("素数でない")
