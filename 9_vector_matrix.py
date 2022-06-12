a9eeb443776bbc8f7e94629bc858b9f410ecab43
SettingTom <tnbt0277@gmail.com>
2022-06-12 22:43:31 +0900

    プログラム9-2(ベクトルの内積)

--- 9_vector_matrix.py
+++ 9_vector_matrix.py
@@ -7,3 +7,13 @@
 # v = [1,2,3,4]
 # u = [5,6,7,8]
 # print(v_plus(v,u))
+
+# プログラム9-2(ベクトルの内積)
+# def in_product(v1,v2):
+#   p = 0
+#   for i in range(len(v1)):
+#     p += v1[i] * v2[i]
+#   return p
+# v = [1,2,3,4]
+# u = [5,6,7,8]
+# print(in_product(v,u))
