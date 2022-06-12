f9ff9a0f8f84319597cea83823c9d553a32b49b5
SettingTom <tnbt0277@gmail.com>
2022-06-12 11:35:33 +0900

    プログラム8-2(√二分法の収束を調べる)、bisection_root.csv作成

--- 8_numerical_calculation.py
+++ 8_numerical_calculation.py
@@ -11,3 +11,18 @@
 #   else:
 #     b = c
 # print(c)
+
+# プログラム8-2(√二分法の収束を調べる)
+# def f(x):
+#   return x**2 - 3
+# file = open("bisection_root3.csv", 'w', encoding='UTF-8')
+# a = 1
+# b = 2
+# for i in range(50):
+#   c = (a+b)/2
+#   if f(c) < 0:
+#     a = c
+#   else:
+#     b = c
+#   file.write(str(i+1) + ',' + str(c) + '\n')
+# file.close()
