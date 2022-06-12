72f8bb0dc4fec979f0fc3b620444cbfb684c2df4
SettingTom <tnbt0277@gmail.com>
2022-06-12 11:28:19 +0900

    プログラム8-1(√3を二分法で求めるプログラム)

---
+++ 8_numerical_calculation.py
@@ -0,0 +1,13 @@
+# プログラム8-1(√3を二分法で求めるプログラム)
+# def f(x):
+#   return x**2 - 3
+# a = 1
+# b = 2
+# eps = 0.000001
+# while b - a > eps:
+#   c = (a+b)/2
+#   if f(c) < 0:
+#     a = c
+#   else:
+#     b = c
+# print(c)
