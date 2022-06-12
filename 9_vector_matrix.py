8e7021aa5b2dd55de960e140253b8f726d5922f5
SettingTom <tnbt0277@gmail.com>
2022-06-12 22:37:36 +0900

    プログラム9-1(ベクトルの和)

---
+++ 9_vector_matrix.py
@@ -0,0 +1,9 @@
+# プログラム9-1(ベクトルの和)
+# def v_plus(v1,v2):
+#   v = []
+#   for i in range(len(v1)):
+#     v.append(v1[i] + v2[i])
+#   return v
+# v = [1,2,3,4]
+# u = [5,6,7,8]
+# print(v_plus(v,u))
