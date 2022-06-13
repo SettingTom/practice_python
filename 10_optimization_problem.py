407f38bc9b8756b00228d75d202bc24f8ba8ae4e
SettingTom <tnbt0277@gmail.com>
2022-06-13 23:46:03 +0900

    プログラム10-2(再起呼び出しによる組み合わせ最適化)

--- 10_optimization_problem.py
+++ 10_optimization_problem.py
@@ -29,3 +29,48 @@
 # print(s1.name + s2.name + s3.name)
 # print("合計値:" + str(s1.price + s2.price + s3.price))
 # print("総満足度:" + str(s1.like + s2.like + s3.like))
+
+# プログラム10-2(再起呼び出しによる組み合わせ最適化)
+# class Snack:
+#   def __init__(self, name, price, like):
+#     self.name = name
+#     self.price = price
+#     self.like = like
+
+# def total_price(sn_list):
+#   total = 0
+#   for x in sn_list:
+#     total += x.price
+#   return total
+
+# def total_like(sn_list):
+#   total = 0
+#   for x in sn_list:
+#     total += x.like
+#   return total
+
+# def print_snack_list(sn_list):
+#   for x in sn_list:
+#     print(x.name)
+#   print("合計" + str(total_price(sn_list)))
+#   print("満足度" + str(total_like(sn_list)))
+
+# def best_list(l, sn_set):
+#   max_like = 0
+#   best1 = l
+#   for s in sn_set:
+#     if total_price(l + [s]) > 300:
+#       continue
+#     r1 = best_list(l + [s], sn_set)
+#     if max_like < total_like(r1):
+#       max_like = total_like(r1)
+#       best1 = r1
+#   return best1
+
+# sn1 = Snack("チョコ", 130, 18)
+# sn2 = Snack("ポテトチップス", 120, 15)
+# sn3 = Snack("クッキー", 80, 12)
+# sn4 = Snack("ラムネ", 30, 4)
+# sn5 = Snack("ガム", 20, 2)
+# sn_set = {sn1, sn2, sn3, sn4, sn5}
+# print_snack_list(best_list([],sn_set))
