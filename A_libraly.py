7589c426493ced4a4887a814ba5ac2e69feca443
SettingTom <tnbt0277@gmail.com>
2022-06-14 22:25:27 +0900

    プログラムA-1(NumPyの利用例)

---
+++ A_libraly.py
@@ -0,0 +1,9 @@
+# プログラムA-1(NumPyの利用例)
+# import numpy as np
+
+# arr1 = np.array([[1,2],[3,4]])
+# arr2 = np.array([[5,6],[7,8]])
+# arr3 = arr1.dot(arr2)
+# print('arr1:',arr1)
+# print('arr2:',arr2)
+# print('arr3:',arr3)
